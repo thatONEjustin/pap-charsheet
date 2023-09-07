93405d632948602baa42691cbfb8fd67a57acb4b
thatONEjustin <justinpaelmo@gmail.com>
2023-09-06 21:56:27 -0500

    On branch main
     Initial commit
    
     Changes to be committed:
    	new file:   .gitignore
    	new file:   .vscode/extensions.json
    	new file:   .vscode/launch.json
    	new file:   README.md
    	new file:   astro.config.mjs
    	new file:   package-lock.json
    	new file:   package.json
    	new file:   public/favicon.svg
    	new file:   src/components/baseLayout.astro
    	new file:   src/components/footer.astro
    	new file:   src/components/forms/form.svelte
    	new file:   src/components/forms/inputField.svelte
    	new file:   src/components/header.astro
    	new file:   src/components/nav.astro
    	new file:   src/env.d.ts
    	new file:   src/pages/character-sheet.astro
    	new file:   src/pages/index.astro
    	new file:   svelte.config.js
    	new file:   tsconfig.json

---
+++ src/components/forms/form.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+  import InputField from './inputField.svelte';
+
+  function validate(event: Event) {
+    console.log(FormData(event.currentTarget))
+  }
+</script>
+
+<form on:submit|preventDefault={validate}>
+  <fieldset>
+        
+      <InputField type="text" label="Player Name" name="player_name" id="player_name" />
+
+      <InputField type="text" label="Character Name" name="character_name" id="character_name" />
+  </fieldset>
+</form>
+
+<style>
+  form {
+    width: auto;
+    max-width: 680px;
+    margin: 0 auto;
+  }
+
+  fieldset {
+    display: flex;
+    flex-direction: column;
+  }
+</style>
---
+++ src/components/forms/inputField.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+  // logic goes here
+  export let editable = false;
+
+  export let value = $$restProps.value;
+
+  let fieldLabel = $$props.label;
+  
+  /** @param {MouseEvent} event */
+  function handleClick(event: Event) {
+    editable = ! editable;
+  }
+</script>
+
+<div>
+  <label for={$$props.name}>{$$props.label}</label>
+  {#if !editable && value }
+  <a href=# on:click={handleClick}>{value}</a>
+  {/if}
+  {#if editable || value == undefined }
+  <input {...$$restProps} />
+  {/if}
+</div>
+
+<style>
+  /* styles go here */
+  div {
+    display: flex;
+    flex-direction: column;
+    margin-bottom: 10px;
+    width: auto;
+  }
+
+  div input {
+    width: initial;
+  }
+</style>
